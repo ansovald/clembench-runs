b236b31be47ae0137286a51ad7be9bc282ffcd91
Karl Osswald <95127530+ansovald@users.noreply.github.com>
2026-03-13 13:53:48 +0100

    cleaned up directories (#97)
    * fixed clean_up scores, added EuroLLM and got-oss-20b
    
    * added runs
    
    * rerun dond
    
    * added experiments
    
    * added Qwen2.5-72B-Instruct
    
    * clean ed up + reran wordle
    
    * fixed clean_up scores, added EuroLLM and got-oss-20b
    
    * clean up dirs

--- check_scores_files.py
+++ check_scores_files.py
@@ -1,45 +1,127 @@
 import os
+import copy
+
+def build_instance_dict(root_folder):
+    # goes through root folder, and builds a dict of the form:
+    # { 
+    #   game_name: {
+    #       experiment_name: [instance_name, ...],
+    #       ...
+    #   },
+    #   ...
+    # }
+    instance_dict = {}
+    out_of_pace_dirs = []
+    for model_name in os.listdir(root_folder):
+        model_path = os.path.join(root_folder, model_name)
+        print(f"Processing model: {model_name}")
+        if os.path.isdir(model_path):
+            for game_name in os.listdir(model_path):
+            # for game_name in ['codenames']:
+                game_path = os.path.join(model_path, game_name)
+                if os.path.isdir(game_path):
+                    if game_name not in instance_dict:
+                        instance_dict[game_name] = {}
+                    for experiment_name in os.listdir(game_path):
+                        experiment_path = os.path.join(game_path, experiment_name)
+                        if os.path.isdir(experiment_path):
+                            if experiment_name not in instance_dict[game_name]:
+                                instance_dict[game_name][experiment_name] = []
+                                print(f"Processing experiment: {experiment_name}")
+                                print(f"Experiment path: {experiment_path}")
+                            for episode_name in os.listdir(experiment_path):
+                                episode_path = os.path.join(experiment_path, episode_name)
+                                if os.path.isdir(episode_path):
+                                    if not episode_name.startswith("instance_"):
+                                        print(f"WARNING: {episode_path} should be an instance folder, but it doesn't start with 'instance_'!")
+                                        out_of_pace_dirs.append(episode_path)
+                                    elif episode_name not in instance_dict[game_name][experiment_name]:
+                                        instance_dict[game_name][experiment_name].append(episode_name)
+    # sort the dict and its nested dicts and lists for consistency
+    instance_dict = dict(sorted(instance_dict.items()))
+    for game_name in instance_dict:
+        instance_dict[game_name] = dict(sorted(instance_dict[game_name].items()))
+        for experiment_name in instance_dict[game_name]:
+            instance_dict[game_name][experiment_name] = sorted(instance_dict[game_name][experiment_name])
+    return instance_dict, out_of_pace_dirs
 
 
-def count_missing_scores(root_folder):
+def count_missing_scores(root_folder, instance_dict):
     missing_scores_count = 0
+    missing_scores = {}
+    # get working dir
+    working_dir = os.getcwd()
+    root_folder = os.path.join(working_dir, root_folder)
 
     # Iterate over each model name folder
     for model_name in os.listdir(root_folder):
         model_path = os.path.join(root_folder, model_name)
+        print(f"Checking model: {model_name}")
+        print(f"Model path: {model_path}")
+
+        model_instance_dict = copy.deepcopy(instance_dict)
+        missing_model_scores = {}
 
         # Check if it's a directory
         if os.path.isdir(model_path):
-            # Iterate over each game name folder
-            for game_name in os.listdir(model_path):
+            for game_name in model_instance_dict:
                 game_path = os.path.join(model_path, game_name)
+                if not os.path.isdir(game_path):
+                    print(f"WARNING: {game_path} should be a directory, but it's not!")
+                    missing_model_scores[game_name] = "Game folder is missing or not a directory"
+                    break
+                    continue
+                for experiment_name in model_instance_dict[game_name]:
+                    experiment_path = os.path.join(game_path, experiment_name)
+                    if not os.path.isdir(experiment_path):
+                        print(f"WARNING: {experiment_path} should be a directory, but it's not!")
+                        if game_name not in missing_model_scores:
+                            missing_model_scores[game_name] = {}
+                        missing_model_scores[game_name][experiment_name] = f"Experiment folder is missing or not a directory : {experiment_path}"
+                        continue
+                    for episode_name in model_instance_dict[game_name][experiment_name]:
+                        episode_path = os.path.join(experiment_path, episode_name)
+                        if not os.path.isdir(episode_path):
+                            print(f"WARNING: {episode_path} should be a directory, but it's not!")
+                            if game_name not in missing_model_scores:
+                                missing_model_scores[game_name] = {}
+                            if experiment_name not in missing_model_scores[game_name]:
+                                missing_model_scores[game_name][experiment_name] = {}
+                            print(missing_model_scores[game_name][experiment_name])
+                            missing_model_scores[game_name][experiment_name][episode_name] = "Episode folder is missing or not a directory"
+                            continue
+                        scores_path = os.path.join(episode_path, 'scores.json')
+                        if not os.path.exists(scores_path):
+                            missing_scores_count += 1
+                            if game_name not in missing_model_scores:
+                                missing_model_scores[game_name] = {}
+                            if experiment_name not in missing_model_scores[game_name]:
+                                missing_model_scores[game_name][experiment_name] = {}
+                            missing_model_scores[game_name][experiment_name][episode_name] = "scores.json is missing"
+                            print(f"Missing 'scores.json' in episode: {episode_path}")
+                        else:
+                            # remove the instance from the dict to keep track of which ones we've seen
+                            model_instance_dict[game_name][experiment_name].remove(episode_name)
+            if missing_model_scores:
+                missing_scores[model_name] = missing_model_scores
 
-                # Check if it's a directory
-                if os.path.isdir(game_path):
-                    # Iterate over each experiment name folder
-                    for experiment_name in os.listdir(game_path):
-                        experiment_path = os.path.join(game_path, experiment_name)
-
-                        # Check if it's a directory
-                        if os.path.isdir(experiment_path):
-                            # Iterate over each episode folder
-                            for episode_name in os.listdir(experiment_path):
-                                episode_path = os.path.join(experiment_path, episode_name)
-
-                                # Check if it's a directory
-                                if os.path.isdir(episode_path):
-                                    # Check if 'scores.json' file is missing
-                                    scores_path = os.path.join(episode_path, 'scores.json')
-                                    if not os.path.exists(scores_path):
-                                        missing_scores_count += 1
-                                        print(f"Missing 'scores.json' in episode: {episode_path}")
-
-    return missing_scores_count
+    return missing_scores_count, missing_scores
 
 
 # Set the root folder path
 root_folder = "v3.0"
 
-# Call the function
-missing_count = count_missing_scores(root_folder)
+import json
+instance_dict, out_of_place_dirs = build_instance_dict(root_folder)
+with open(os.path.join(root_folder, 'instance_dict.json'), 'w') as f:
+    json.dump(instance_dict, f, indent=4)
+
+print(f"Out of place directories: {len(out_of_place_dirs)}")
+for dir in out_of_place_dirs:
+    print(dir)
+
+missing_count, missing_scores = count_missing_scores(root_folder, instance_dict)
 print(f"Total number of episode folders without 'scores.json': {missing_count}")
+if missing_scores:
+    with open(os.path.join(root_folder, 'missing_scores.json'), 'w') as f:
+        json.dump(missing_scores, f, indent=4)
